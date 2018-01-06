cef5b92db45208f396ac201468b943c2c7bff274
root <root@raspberrypi>
2018-01-06 01:20:43 +0100

    commit6

--- src/test/java/TestClassicWritting.java
+++ src/test/java/TestClassicWritting.java
@@ -11,17 +11,17 @@ import main.App;
  * Classic test for simple App. It contains to test methods.
  * A TestSuite is created by getting via reflect API all methods whose name start with "test"
  */
-public class ClassicTestWritting  extends TestCase
+public class TestClassicWritting  extends TestCase
 {
 	App app;
 
 	// Retourne un objet qui contient la suite d'objet TestCase
 	// Chaque objet TestCase correspond à un test, i.e une méthode de test de ce TestCase (ce sont les méthodes test ...())
 	// Cette méthode statique n'est pas utilisée par JUnit, mais peut être par maven
-    public static Test suite() { return new TestSuite(ClassicTestWritting.class); }
+    public static Test suite() { return new TestSuite(TestClassicWritting.class); }
 
      // Invoqué par lors de la construction de la suite de tests
-    public ClassicTestWritting(String testName)
+    public TestClassicWritting(String testName)
     {
         super(testName); // testName correspond au nom d'une méthode de test de ce TestCase
     }
